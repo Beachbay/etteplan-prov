ad744476c268375ab1587537d3c0fe3c5587bf0f
Beachbay <113332198+Beachbay@users.noreply.github.com>
2023-03-23 14:46:57 +0100

    Initial commit

---
+++ EtteplanProv/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Transactions;
+using System.Xml;
+
+XmlDocument document = new XmlDocument();
+document.Load("C:\\Users\\filip\\OneDrive\\Skrivbord\\sma_gentext.xml");
+
+XmlNode? node = document.SelectSingleNode("/root/file/body/trans-unit[@id='42007']");
+
+if (node != null)
+{
+    XmlNode? targetNode = node.SelectSingleNode("target");
+
+    if (targetNode != null)
+    {
+        Console.WriteLine(targetNode.InnerText);
+    }
+}
+else if (node == null)
+{
+    Console.WriteLine("No target found");
+}
+
